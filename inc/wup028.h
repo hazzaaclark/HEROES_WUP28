9eb20f8c0a33341edf889d9183b78eb945c675ae
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-07-05 17:40:10 +0100

    Add files via upload

---
+++ inc/wup028.h
@@ -0,0 +1,28 @@
+/* Copyright (C) 2023 Harry Clark */
+
+/* dinput8 Implementation of WUP-028 for Sonic Heroes */
+
+/* THIS FILE PERTAINS TO THE FUNCTIONALITY OF THE WUP-028 DRIVER */
+
+#ifndef WUP_028
+#define WUP_028
+
+/* NESTED INCLUDES */
+
+#include "common.h"
+#include "dinput.h"
+
+/* SYSTEM INCLUDES */
+
+#include <stdio.h>
+
+#if defined(INIT_WUP28)
+#define INIT_WUP28
+#else
+#define INIT_WUP28
+
+
+
+#endif
+
+#endif

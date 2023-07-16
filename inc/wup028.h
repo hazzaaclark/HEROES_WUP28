1e74d636fb7b4285eff22429a55511f901545724
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-07-16 23:32:43 +0100

    added gcn/wii ipc macros

--- inc/wup028.h
+++ inc/wup028.h
@@ -17,12 +17,52 @@
 #include <stdio.h>
 
 #if defined(INIT_WUP28)
-#define INIT_WUP28
+#define INIT_WUP28 
 #else
 #define INIT_WUP28
 
+#define WUP_ID                             0x57E03337 /* HARDWARE ID PROVIDED BY DEVICE MANAGER */
+#define WUP_SIZE                           0x25
+
+#define WUP_ALIGN __attribute__((aligned(32)))
+#define WUP_PORT                             4
+#define WUP_TRIGGER                        170
+
+/* DEFINE THE TIME IN MILLISECONDS TO REPRESENT */
+/* THE DISCONNECTION */
+
+#define WUP_TIME *(243000/4)
+#define WUP_DISCONNECT (1500 WUP_TIME)
+
+/* TAKING INTO ACCOUNT THE IPC SCHEMA FROM THE WII */
+/* IPC DICTATES THE FUNCTIONALITY THAT SERVES AS THE IO FOR THE WII */
+/* DETERMINING THE FLAGS AND REGISTERS AND IRQ'S NECESSARY FOR HARDWARE INITIALISATION */
+
+/* THE FOLLOWING IS THE IPC VECTOR TABLE */
+/* SEE: https://github.com/dolphin-emu/dolphin/blob/4e46be951a3872b03352c82e4aa3def27de4f124/Source/Core/Core/IPC_HLE/WII_IPC_HLE.h */
+
+#ifdef IOS_IPC
+#define IOS_IPC
+#else
+#define IOS_IPC
+
+#define IPC_SUCCESS                          (1 << 0)
+#define IPC_DENIED                           (1 << -1)
+#define IPC_FILE                             (1 << -2)
+#define IPC_INVALID_ARG                      (1 << -4)
+#define IPC_NF                               (1 << -6)
+#define IPC_BUSY                             (1 << -8)
+#define IPC_EIO                              (1 << -12)
+#define IPC_ALLOC_FAIL                       (1 << -22)
+#define IPC_FATAL                            (1 << -101)
+#define IPC_PERM_DENIED                      (1 << -102)
+
+#define IPC_OPEN_NONE                                                       0
+#define IPC_OPEN_READ                                                       1
+#define IPC_OPEN_WRITE                                                      2
+#define IPC_OPEN_RW(READ, WRITE)             (IPC_OPEN_READ + IPC_OPEN_WRITE)
 
 
 #endif
-
+#endif
 #endif

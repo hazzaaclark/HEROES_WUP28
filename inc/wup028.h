e6562a487951e738881784e9f9454c081a157054
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-07-25 18:05:15 +0100

    removed redefinitions

--- inc/wup028.h
+++ inc/wup028.h
@@ -112,12 +112,10 @@ typedef struct PAD
 
 static bool(*CHECK_ACCESS(DEVICE* DEVICE));
 typedef void(*ADD_WUP_ADAPTER(DEVICE* DEVICE));
-typedef void(*RESET(void));
-typedef void(*SETUP(void));
 
 typedef void(DINPUT_STDCALL* INPUT_CALLBACK)(DEVICE* INPUT_DEVICE, void*);
 typedef U32 DINPUT_STDCALL* GET_CONFIG_DESCRIPTOR(DEVICE* DEVICE, U8 VALUE, 
-	                                             DEVICE_CFG::CONFIGURE_DEVICE* CONFIG);
+	                                             DEVICE_CFG::CONFIGURE_DEVICE CONFIG);
 
 #endif
 #endif
